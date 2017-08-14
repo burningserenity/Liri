e703b019f59301f0301e2ecf9dd881d7fba81932
burningserenity <burningserenity@bitmai.la>
2017-08-14 18:07:46 -0400

    Added initial files

---
+++ keys.js
@@ -0,0 +1,15 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: 'iWC0whD6iUKWsXV27VsXDb7Yr',
+    consumer_secret: 'ssT5HUqzdRskxd1zW6AFm0XHbEQrZ0xX8SfTr3mryiJpOhssXz',
+    access_token_key: '879096250028765184-kU5JLXbPRLMM38V9OGAAgE4NnuFG9tu',
+    access_token_secret: 'xVq81QIge8pTCLfs5zBRg9SUJ8A1AOZRBgvcc8Sfj7i4R'
+};
+
+exports.spotifyKeys = {
+    client_id: 'adf92fb3bb1a4c0ab73c8ed2df48df17',
+    client_secret: '62f06508755d43c288d16db901bf91e5'
+};
+
+exports.omdbKey = 40e9cece;
---
+++ liri.js
@@ -0,0 +1,6 @@
+// TODO: Grab keys from keys.js
+// take the following commands:
+// my-tweets --> show your last 20 tweets with creation time
+// spotify-this-song --> add argument '<song name>'; show the Artist(s), song name, preview link of the song from Spotify, the album the song is from. If no song specified, default to "The Sign" by Ace of Base.
+// movie-this -->  add argument '<movie name>'; show the movie title, year of release, IMDB rating, Rotten Tomatoes Rating, country of origin, language, plot, and actors in movie. If no movie specified, choose "Mr. Nobody."
+// do-what-it-says

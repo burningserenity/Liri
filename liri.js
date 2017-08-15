e05d73bf533b0fb4830c8126954391493e7b219d
burningserenity <burningserenity@bitmai.la>
2017-08-15 14:42:17 -0400

    Added Spotify search function

--- liri.js
+++ liri.js
@@ -14,11 +14,31 @@ if (process.argv[2] === "my-tweets") {
             console.log(tweets[i].text);
             }
         }
+        else console.log("Error occurred: " + error)
     });
 }
 // spotify-this-song --> add argument '<song name>'; show the Artist(s), song name, preview link of the song from Spotify, the album the song is from. If no song specified, default to "The Sign" by Ace of Base.
 else if (process.argv[2] === "spotify-this-song") {
     var spotifyKeys = keys.spotifyKeys;
+    var spotify = require('node-spotify-api');
+    var client = new spotify({
+        id: spotifyKeys.client_id,
+        secret: spotifyKeys.client_secret
+    });
+    var song;
+    if (process.argv.length < 4) song = 'The Ace of Base';
+    else song = process.argv.slice(3, process.argv.length);
+    client.search({type: 'track', query: song}, function(err, data){
+        if (err) return console.log('Error occurred: ' + err);
+        var returnTrack = data.tracks.items[0];
+        console.log("Artist(s):");
+        for (i = 0; i < returnTrack.artists.length; i++) {
+            console.log(returnTrack.artists[i].name);
+        }
+        console.log(returnTrack.name);
+        console.log(returnTrack.preview_url);
+        console.log(returnTrack.album.name)
+    });
 }
 // movie-this -->  add argument '<movie name>'; show the movie title, year of release, IMDB rating, Rotten Tomatoes Rating, country of origin, language, plot, and actors in movie. If no movie specified, choose "Mr. Nobody."
 else if (process.argv[2] === "movie-this") {
